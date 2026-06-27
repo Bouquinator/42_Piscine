e72e2aa3565a058c9756fc621a8cf657055aa5a9
Mael Grossenbacher <mgrossen@c1r13s2.42lausanne.ch>
2026-06-27 20:49:53 +0200

    C02 ex05

--- C/C02/ex05/ft_str_is_uppercase.c
+++ C/C02/ex05/ft_str_is_uppercase.c
@@ -6,8 +6,29 @@
 /*   By: mgrossen <marvin@42.fr>                       +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2026/06/27 17:14:24 by mgrossen       #+#    #+#                */
-/*   Updated: 2026/06/27 17:16:26 by mgrossen       ########   odam.nl        */
+/*   Updated: 2026/06/27 20:49:02 by mgrossen       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_str_is_uppercase(char *str)
+{
+	int	i;
 
+	i = 0;
+	while (str[i] != 0)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			i++;
+		else
+			return (0);
+	}
+	return(1);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%d",ft_str_is_uppercase("UUUu"));
+	return (0);
+}
