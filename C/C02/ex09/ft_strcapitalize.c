9e6aba4adafce4a68ec5b2ba34bb33ba3806c13f
Mael Grossenbacher <mgrossen@c1r14s1.42lausanne.ch>
2026-06-28 11:40:18 +0200

    C02 ex09 working

--- C/C02/ex09/ft_strcapitalize.c
+++ C/C02/ex09/ft_strcapitalize.c
@@ -6,23 +6,48 @@
 /*   By: mgrossen <marvin@42.fr>                       +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2026/06/27 23:35:00 by mgrossen       #+#    #+#                */
-/*   Updated: 2026/06/28 01:31:39 by mgrossen       ########   odam.nl        */
+/*   Updated: 2026/06/28 11:33:55 by mgrossen       ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_is_it(char	letter)
+int	ft_what(char letter)
 {
 	if (letter >= '0' && letter <= '9')
-		return(2);
+		return(1);
 	else if (letter >= 'a' && letter <= 'z')
-		return (3);
+		return (2);
 	else if (letter >= 'A' && letter <= 'Z')
 		return (3);
 	else
-		return (1);
+		return (0);
 }
 
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	a;
+	int	b;
 
+	i = 1;
+	if (ft_what(str[0]) == 2)
+			str[0] = str[0] + ('A' - 'a');
+	while (str[i] != 0)
+	{
+		a = ft_what(str[i]);
+		b = ft_what(str[(i-1)]);
+		if (a == 2 && b == 0)
+			str[i] = str[i] + ('A' - 'a');
+		else if (b == 1 || b == 2 || b == 3)
+		{
+			if (a == 3)
+				str[i] = str[i] + ('a' - 'A');
+		}
+		i++;
+	}
+	return(str);
+}
+
+/*
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
@@ -43,13 +68,14 @@ char	*ft_strcapitalize(char *str)
 	}
 	return (str);
 }
+*/
 
 #include <stdio.h>
 #include <string.h>
 
 int	main(void)
 {
-	char	string[] = "AAA DDD sSs";
+	char	string[] = "hi, how are you? 42words forty-two; fifty+and+one";
 	
 	printf("%s", ft_strcapitalize(string));
 }
